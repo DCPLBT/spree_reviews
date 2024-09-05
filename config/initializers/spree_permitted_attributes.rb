a3981e1d95ccfcb5532aa7175b38f5aa1982a156
Tashi Dendup <tashii.dendupp@gmail.com>
2024-09-05 20:13:24 +0600

    Update spree_permitted_attributes.rb

--- config/initializers/spree_permitted_attributes.rb
+++ config/initializers/spree_permitted_attributes.rb
@@ -1,7 +1,9 @@
 module Spree
   module PermittedAttributes
-    ATTRIBUTES += %i[review_attributes feedback_review_attributes]
-    mattr_reader *ATTRIBUTES
+    ATTRIBUTES << :review_attributes
+    ATTRIBUTES << :feedback_review_attributes
+    
+    mattr_reader(*ATTRIBUTES)
 
     @@product_attributes += %i[avg_rating reviews_count]
     @@review_attributes = [:rating, :title, :review, :name, :show_identifier]
